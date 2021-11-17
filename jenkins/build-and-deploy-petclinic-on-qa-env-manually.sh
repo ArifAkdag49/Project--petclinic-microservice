b9011bc3cda7da280597ac393597b2d7d3bc472d
“DatBy” <arifakdag25@gmail.com>
2021-11-17 09:13:16 +0000

    added script for jenkins job to build and deploy app on QA environment

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,6 +1,6 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-APP_REPO_NAME="clarusway-repo/petclinic-app-qa"
+APP_REPO_NAME="-repo/petclinic-app-qa"
 APP_STACK_NAME="Call-petclinic-App-QA-1"
 CFN_KEYPAIR="call-petclinic-qa.key"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
